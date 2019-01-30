ee9ea35bbbe8d0dcac529f5d0213f23cc080c156
Bubba <rob@stechstudio.com>
2019-01-30 14:56:07 -0500

    Implement `containsEvent()` and `getContainedEvent()` on SNS events

--- src/Events/Sns.php
+++ src/Events/Sns.php
@@ -4,7 +4,41 @@ namespace STS\AwsEvents\Events;
 
 class Sns extends Event
 {
-
     protected static $contains = 'Records.Sns';
+    protected $containedEvent = null;
+
+    /**
+     * @return Event|null
+     */
+    public function getContainedEvent(): ?Event
+    {
+        if ($this->containsEvent()) {
+            return $this->containedEvent;
+        }
+        return null;
+    }
+
+    /**
+     * @param String $rawEvent
+     * @return bool
+     */
+    public function containsEvent(String $rawEvent): bool
+    {
+        if (!is_null($this->containedEvent)) {
+            return true;
+        }
+        $event = null;
+        try {
+            $event = Event::make($rawEvent);
+        } catch (\JsonException $e) {
+            return false;
+        }
+
+        if (is_subclass_of($event, Event::class)) {
+            $this->containedEvent = $event;
+            return true;
+        }
+        return false;
+    }
 
 }
