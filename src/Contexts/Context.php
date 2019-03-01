51925f94950e81a9b6f9fb54ad5bd40ea183fd5d
Bubba <rob@stechstudio.com>
2019-03-01 15:28:11 -0500

    Generate this properly

--- src/Contexts/Context.php
+++ src/Contexts/Context.php
@@ -93,16 +93,16 @@ class Context
         $contextCollection = new Collection(json_decode($jsonContext, true));
         $context = new static;
 
-        $context->setFunctionName(env('AWS_LAMBDA_FUNCTION_NAME'));
-        $context->setFunctionVersion(env('AWS_LAMBDA_FUNCTION_VERSION'));
-        $context->setLogGroupName(env('AWS_LAMBDA_LOG_GROUP_NAME'));
-        $context->setLogStreamName(env('AWS_LAMBDA_LOG_STREAM_NAME'));
+        $context->setFunctionName(env('AWS_LAMBDA_FUNCTION_NAME', ''));
+        $context->setFunctionVersion(env('AWS_LAMBDA_FUNCTION_VERSION', ''));
+        $context->setLogGroupName(env('AWS_LAMBDA_LOG_GROUP_NAME', ''));
+        $context->setLogStreamName(env('AWS_LAMBDA_LOG_STREAM_NAME', ''));
 
-        $context->setMemoryLimitInMb($contextCollection->get(strtolower('')));
-        $context->setInvokedFunctionArn($contextCollection->get(strtolower('Lambda-Runtime-Invoked-Function-Arn')));
-        $context->setAwsRequestId($contextCollection->get('lambda-runtime-aws-request-id'));
-        $context->setRuntimeDeadlineMs($contextCollection->get(strtolower('Lambda-Runtime-Deadline-Ms')));
-        $context->setXRayTraceId($contextCollection->get(strtolower('Lambda-Runtime-Trace-Id')));
+        $context->setMemoryLimitInMb(env('AWS_LAMBDA_FUNCTION_MEMORY_SIZE', ''));
+        $context->setInvokedFunctionArn($contextCollection->get(strtolower('Lambda-Runtime-Invoked-Function-Arn'), ''));
+        $context->setAwsRequestId($contextCollection->get('lambda-runtime-aws-request-id', ''));
+        $context->setRuntimeDeadlineMs((int) $contextCollection->get(strtolower('Lambda-Runtime-Deadline-Ms'), 0));
+        $context->setXRayTraceId($contextCollection->get(strtolower('Lambda-Runtime-Trace-Id'), ''));
 
         return $context;
     }
